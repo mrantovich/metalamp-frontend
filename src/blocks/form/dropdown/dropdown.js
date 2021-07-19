183d4d4b590e36279c216a61463f3e98cd61abcc
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-19 21:51:03 +0700

    Make relative .dropdown itself instead of .dropdown__field to not allow dropdown menu disappear

--- src/blocks/form/dropdown/dropdown.js
+++ src/blocks/form/dropdown/dropdown.js
@@ -4,15 +4,18 @@ dropdownField.addEventListener('click', makeDropdownMenuVisible);
 
 function makeDropdownMenuVisible() {
 
-    let dropdownMenu = this.firstElementChild;
-    let hiddenMarker = 'dropdown__menu_is-hidden';
-    let visibleMarker = 'dropdown__menu_is-visible';
+    let dropdownMenu = this.nextElementSibling;
+    let dropdownMenuIsExpanded = 'dropdown__field_is-expanded';
+    let hiddenFlag = 'dropdown__menu_is-hidden';
+    let visibleFlag = 'dropdown__menu_is-visible';
 
-    if (dropdownMenu.classList.contains(hiddenMarker)) {
-        dropdownMenu.classList.remove(hiddenMarker);
-        dropdownMenu.classList.add(visibleMarker);
+    if (dropdownMenu.classList.contains(hiddenFlag)) {
+        dropdownMenu.classList.remove(hiddenFlag);
+        dropdownMenu.classList.add(visibleFlag);
+        this.classList.add(dropdownMenuIsExpanded);
     } else {
-        dropdownMenu.classList.remove(visibleMarker);
-        dropdownMenu.classList.add(hiddenMarker);
+        dropdownMenu.classList.remove(visibleFlag);
+        dropdownMenu.classList.add(hiddenFlag);
+        this.classList.remove(dropdownMenuIsExpanded);
     };
 };
