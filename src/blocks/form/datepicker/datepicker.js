85af37b6058f4c3ff0efb88f50eb1472930bd9e2
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-24 14:10:08 +0700

    Using litepicker instead of js-datepicker, styling first one

--- src/blocks/form/datepicker/datepicker.js
+++ src/blocks/form/datepicker/datepicker.js
@@ -1,4 +1,22 @@
-import datepicker from 'js-datepicker';
-import 'js-datepicker/dist/datepicker.min.css';
+import Litepicker from "litepicker";
 
-const picker = datepicker('.datepicker');
+const datePickerParentElement = document.querySelector('.datepicker__wrapper');
+const datePickerElement = document.querySelector('.datepicker');
+
+const picker = new Litepicker({
+    element: datePickerElement,
+    parentEl: datePickerParentElement,
+    lang: 'ru-RU',
+    singleMode: false,
+    buttonText: {
+        'previousMonth': '',
+        'nextMonth': ''
+    }
+});
+
+picker.on('show', () => {
+    let litePicker = document.querySelector('.litepicker');
+    console.log(litePicker);
+    litePicker.style.top = 'auto';
+    litePicker.style.left = '0';
+});
