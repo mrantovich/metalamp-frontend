7a2a1870e4e454e5d399418aa306db844b30dcbb
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-24 11:11:41 +0700

    Including js-datepicker

---
+++ src/blocks/form/datepicker/datepicker.js
@@ -0,0 +1,4 @@
+import datepicker from 'js-datepicker';
+import 'js-datepicker/dist/datepicker.min.css';
+
+const picker = datepicker('.datepicker');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './blocks/form/dropdown/dropdown';
 import './blocks/form/like-button/like-button';
 import './blocks/form/range-slider/range-slider';
+import './blocks/form/datepicker/datepicker';
 
 import './main/main.pug';
 import './main/main.scss';
