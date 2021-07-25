03c5eabbe3129ec3794596ff369702b4529a476a
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-25 11:23:48 +0700

    Masked text-field, using imask.js

--- src/blocks/form/imask.js
+++ src/blocks/form/imask.js
@@ -6,7 +6,7 @@ let maskOptionsForDate = {
     lazy: false,
     autofix: true,
     blocks: {
-        d: {mask: IMask.MaskedRange, placeholderChar: 'Д', from: 1, to: 32, maxLength: 2},
+        d: {mask: IMask.MaskedRange, placeholderChar: 'Д', from: 1, to: 31, maxLength: 2},
         m: {mask: IMask.MaskedRange, placeholderChar: 'М', from: 1, to: 12, maxLength: 2},
         Y: {mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1990, to: 2999, maxLength: 4},
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as $ from 'jquery';
 import './blocks/form/dropdown/dropdown';
 import './blocks/form/like-button/like-button';
+import './blocks/form/imask';
 import './blocks/form/range-slider/range-slider';
 import './blocks/form/datepicker/datepicker';
 
