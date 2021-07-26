294dd94f26253ca76fbf3d50a063408cc0428684
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-26 22:18:17 +0700

    Adding declination to dropdown-selection

--- src/blocks/form/dropdown/dropdown.js
+++ src/blocks/form/dropdown/dropdown.js
@@ -1,6 +1,15 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
+function declOfNum(n, text_forms) {  
+    n = Math.abs(n) % 100; 
+    var n1 = n % 10;
+    if (n > 10 && n < 20) { return text_forms[2]; }
+    if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+    if (n1 == 1) { return text_forms[0]; }
+    return text_forms[2];
+};
+
 let dropdowns = $('.iqdropdown');
 
 for (let i = 0; i < dropdowns.length; i++) {
@@ -10,6 +19,7 @@ for (let i = 0; i < dropdowns.length; i++) {
             selectionText: 'гость',
             setSelectionText: function(itemCount, totalItems) {
                 let text = this.selectionText;
+                text = declOfNum(totalItems, ['гость', 'гостя', 'гостей']);
                 return `${totalItems} ${text}`;
             },
         });
