c1069f7b386313cf5541ac861af2290241736228
Mikhail Rantovich <mrantovich@gmail.com>
2021-07-26 21:55:42 +0700

    Making so-called single dropdown

--- src/blocks/form/dropdown/dropdown.js
+++ src/blocks/form/dropdown/dropdown.js
@@ -5,23 +5,43 @@ let dropdowns = $('.iqdropdown');
 
 for (let i = 0; i < dropdowns.length; i++) {
     let dr = dropdowns[i];
-    $(dr).iqDropdown({
+    console.log(dr.className.includes('iqdropdown_single'));
+    if (dr.className.includes('iqdropdown_single')) {
+        $(dr).iqDropdown({
+            setSelectionText: function(itemCount, totalItems) {
+                //return `${totalItems}`;
+                console.log('sdfsd')
+            },
+        });
+    } else {
+        $(dr).iqDropdown({
+            setSelectionText: function(itemCount, totalItems) {
+                let textArray = [];
+                let text;
+                let itemKeys = Object.keys(itemCount);
+                let dropdownItems = $(dr).find('.iqdropdown-item')
+        
+                for (let i = 0; i < itemKeys.length;) {
+                    let itemName = dropdownItems[i].innerHTML;
+                    let itemQuant = itemCount[itemKeys[i]];
+                    text = `${itemQuant} ${itemName}`;
+                    textArray.push(text);
+                    i++
+                };
+                let textToDisplay = textArray.join(', ')
+                return `${textToDisplay}`;
+            },
+        });
+    };
+};
+
+
+/* for (let i = 0; i < singleDropdowns.length; i++) {
+    let sdr = singleDropdowns[i];
+    $(sdr).iqDropdown({
         setSelectionText: function(itemCount, totalItems) {
-            let textArray = [];
-            let text;
-            let itemKeys = Object.keys(itemCount);
-            let dropdownItems = $(dr).find('.iqdropdown-item')
-    
-            for (let i = 0; i < itemKeys.length;) {
-                console.log(itemKeys[i])
-                let itemName = dropdownItems[i].innerHTML;
-                let itemQuant = itemCount[itemKeys[i]];
-                text = `${itemQuant} ${itemName}`;
-                textArray.push(text);
-                i++
-            };
-            let textToDisplay = textArray.join(', ')
-            return `${textToDisplay}`;
+            //return `${totalItems}`;
+            console.log('sdfsd')
         },
     });
-};
+}; */
