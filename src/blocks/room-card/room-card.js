8228abe7b0305a3bbb0ae5739a493142965edd2d
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-01 11:19:44 +0700

    Commenting code for room-card

--- src/blocks/room-card/room-card.js
+++ src/blocks/room-card/room-card.js
@@ -1,9 +1,11 @@
+// Slider (or carousel) to slide room images on small cards.
 import Swiper from 'swiper';
 import SwiperCore, {Navigation, Pagination} from 'swiper/core';
 import 'swiper/swiper-bundle.css';
 
 SwiperCore.use([Navigation, Pagination]);
 
+// Each slider is under .room-card__swiper container, so select them all.
 const swipers = document.querySelectorAll('.room-card__swiper');
 
 for (let i = 0; i < swipers.length; i++) {
