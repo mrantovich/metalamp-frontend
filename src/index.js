f313224ba08f470b5af12d0e92dca8adda1b7fba
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-03 02:33:49 +0700

    Making responsive menu on landing page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import * as $ from 'jquery';
 import './blocks/form/dropdown/dropdown';
 import './blocks/form/like-button/like-button';
 import './blocks/form/text-field/text-field';
-import './blocks/form/range-slider/range-slider';
+//import './blocks/form/range-slider/range-slider';
 import './blocks/form/datepicker/datepicker';
 import './blocks/form/expandable-list/expandable-list';
 import './blocks/form/form.js';
 import './blocks/room-card/room-card';
+import './blocks/header/header';
 
 import './main/main.pug';
 import './main/main.scss';
