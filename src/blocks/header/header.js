cbe40cd265eaeefed60b4e0e654e62d64b6b5e93
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-03 14:06:50 +0700

    Positioning burger, locking body when menu is active

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -3,9 +3,15 @@
 const headerOverlay = document.querySelector('.header__overlay');
 const burger = document.querySelector('.header__burger');
 
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+
 burger.addEventListener('click', toggleMenuActive);
 
 function toggleMenuActive() {
     this.classList.toggle('is-active');
     headerOverlay.classList.toggle('is-active');
+
+    html.classList.toggle('is-locked');
+    body.classList.toggle('is-locked');
 };
