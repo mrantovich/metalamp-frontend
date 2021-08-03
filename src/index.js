d1bb1a8f0fa2b56dc5bebd5e40e76b3b9c46436f
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-03 20:42:18 +0700

    Construct search room page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as $ from 'jquery';
 import './blocks/form/dropdown/dropdown';
 import './blocks/form/like-button/like-button';
 import './blocks/form/text-field/text-field';
-//import './blocks/form/range-slider/range-slider';
+import './blocks/form/range-slider/range-slider';
 import './blocks/form/datepicker/datepicker';
 import './blocks/form/expandable-list/expandable-list';
 import './blocks/form/form.js';
