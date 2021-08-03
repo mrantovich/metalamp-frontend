bbe2358165374abcb37e0edcf2057b15afd4c97c
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-03 14:02:30 +0700

    Responsiveness for landing page

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,11 +1,11 @@
 // Work with burger.
 
-const header = document.querySelector('.header');
+const headerOverlay = document.querySelector('.header__overlay');
 const burger = document.querySelector('.header__burger');
 
 burger.addEventListener('click', toggleMenuActive);
 
 function toggleMenuActive() {
     this.classList.toggle('is-active');
-    header.classList.toggle('is-active');
+    headerOverlay.classList.toggle('is-active');
 };
