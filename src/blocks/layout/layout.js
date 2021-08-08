f526b226831eaa8899ad49f240bf3c101fb51547
Mikhail Rantovich <mrantovich@gmail.com>
2021-08-08 13:24:21 +0700

    Auto change page backgrounds

---
+++ src/blocks/layout/layout.js
@@ -0,0 +1,22 @@
+const layoutBackgrounds = document.querySelector('.layout__background-overlay');
+
+function changeLayoutBackground() {
+    let backgrounds = layoutBackgrounds.childNodes;
+    let i = 0;
+    backgrounds[i].style.opacity = 1;
+    setInterval(function() {
+        if (i === 0) {
+            backgrounds[backgrounds.length-1].style.opacity = 0;
+        } else {
+            backgrounds[i-1].style.opacity = 0;
+        };
+        i++;
+        if (i === backgrounds.length) {
+            i = 0;
+            backgrounds[backgrounds.length-1].style.opacity = 0;
+        };
+        backgrounds[i].style.opacity = 1;
+    }, 5000);
+}
+
+changeLayoutBackground();
